66b7719eb9dd099dc4a9afa15c29a53d71b45f90
Arely Gutiérrez <yleraycnan@hotmail.com>
2023-02-12 14:29:42 -0600

    headar and images

--- src/components/Body.js
+++ src/components/Body.js
@@ -7,10 +7,10 @@ import photo4 from '../images/photo4.png';
 export function Body(){
     return(
         <body className='photoSection'>
-            {/* <img src={photo1} className="photoSection1" alt="photoSection" />
+            <img src={photo1} className="photoSection1" alt="photoSection" />
             <img src={photo2} className="photoSection2" alt="photoSection" />
             <img src={photo3} className="photoSection3" alt="photoSection" />
-            <img src={photo4} className="photoSection4" alt="photoSection" /> */}
+            <img src={photo4} className="photoSection4" alt="photoSection" />
 
         </body>
     )
